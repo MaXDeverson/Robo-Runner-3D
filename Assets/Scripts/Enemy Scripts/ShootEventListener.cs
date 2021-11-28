0ac5fe856ed40516fb61a8932daa902e6ab4672f
MaXDeverson <max791194091@gmail.com>
2021-11-28 20:20:25 +0200

    Awesome mines

---
+++ Assets/Scripts/Enemy Scripts/ShootEventListener.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEventListener : MonoBehaviour
+{
+    public void ShootEvent(float inputF)
+    {
+
+    }
+}
